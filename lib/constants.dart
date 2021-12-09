d0902c8ef0942a95b13290eee8d59822b58aa60b
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-10 00:31:22 +0200

    added text styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
+// Colour styles
 const primaryColour = Color.fromRGBO(31, 204, 121, 1);
 const secondaryColour = Color.fromRGBO(255, 100, 100, 1);
 const mainText = Color.fromRGBO(41, 62, 93, 1);
 const secondaryText = Color.fromRGBO(159, 165, 192, 1);
 
+// Text styles
+const int headingOne = 22;
+const int headingTwo = 17;
+const int headingThree = 15;
 
+const int paragraphOne = 17;
+const int paragraphTwo = 15;
+const int smallText = 12;
