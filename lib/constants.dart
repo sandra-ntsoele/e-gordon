40cae4f28bdb2491c8e57ac00186f92c59099c7b
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-10 02:38:08 +0200

    add outline colour and rename other colour styles for better readability

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 // Colour styles
-const primaryColour = Color.fromRGBO(31, 204, 121, 1);
-const secondaryColour = Color.fromRGBO(255, 100, 100, 1);
-const mainText = Color.fromRGBO(41, 62, 93, 1);
-const secondaryText = Color.fromRGBO(159, 165, 192, 1);
+const Color primaryColour = Color.fromRGBO(31, 204, 121, 1);
+const Color secondaryColour = Color.fromRGBO(255, 100, 100, 1);
+const Color mainTextColour = Color.fromRGBO(41, 62, 93, 1);
+const Color secondaryTextColour = Color.fromRGBO(159, 165, 192, 1);
+const Color outlineColour = Color.fromRGBO(208, 219, 234, 1);
 
 // Text styles
 const double headingOne = 22;
