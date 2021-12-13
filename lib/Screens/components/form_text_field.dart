de64b914ae7e9b6e042b82f6d7cd5fdc854b1e62
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-13 14:57:55 +0200

    create: a TextFormField
    To be used for fields that are part of form such as email, food name etc.

---
+++ lib/Screens/components/form_text_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class FormTextField extends StatelessWidget {
+  String? labelText;
+  IconData? prefixIcon;
+
+  FormTextField({
+    Key? key,
+    required this.labelText,
+    required this.prefixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: labelText,
+          prefixIcon: Icon(prefixIcon),
+          labelStyle: const TextStyle(color: secondaryTextColour),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(100)),
+            borderSide: BorderSide(color: outlineColour),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(100)),
+            borderSide: BorderSide(color: outlineColour),
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        ),
+      ),
+    );
+  }
+}
