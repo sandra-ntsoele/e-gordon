70f55e811c4bdfaebe1f31d7faa800f7876c73c8
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-13 16:59:01 +0200

    added: password rules section

--- lib/Screens/sign_up/sign_up.dart
+++ lib/Screens/sign_up/sign_up.dart
@@ -1,8 +1,10 @@
 import 'package:e_gordon/Screens/components/form_text_field.dart';
 import 'package:e_gordon/Screens/components/main_text.dart';
+import 'package:e_gordon/Screens/components/password_rule.dart';
 import 'package:e_gordon/Screens/components/secondary_text.dart';
 import 'package:e_gordon/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -27,6 +29,26 @@ class SignUp extends StatelessWidget {
             labelText: "Password",
             prefixIcon: Icons.lock_outline,
           ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Your password must contain:",
+                style: TextStyle(
+                  color: mainTextColour,
+                  fontSize: paragraphOne,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(
+                children: [
+                  PasswordRule(rule: "Atleast 8 characters,"),
+                  PasswordRule(rule: "Including 1 letter,"),
+                  PasswordRule(rule: "And 1 number"),
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
