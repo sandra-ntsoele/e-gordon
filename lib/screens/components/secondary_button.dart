73667ceb00e09c317607e4d93de5f5e162f8ca46
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-14 15:31:19 +0200

    created: secondary button

---
+++ lib/screens/components/secondary_button.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:e_gordon/constants.dart';
+
+class SecondaryButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  IconData? btnIcon;
+  final bool isGhostButton;
+
+  SecondaryButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.btnIcon,
+    required this.isGhostButton,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ElevatedButton(
+      onPressed: () {
+        onPressed.call();
+      },
+      child: SizedBox(
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (btnIcon != null) ...[
+              Icon(btnIcon),
+              SizedBox(
+                width: size.width * 0.03,
+              ),
+            ],
+            Text(
+              text,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      style: TextButton.styleFrom(
+        textStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+        // onPrimary: mainTextColour,
+        backgroundColor: Colors.white,
+        primary: outlineColour,
+        fixedSize: const Size.fromHeight(56),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+          side: BorderSide(
+            color: outlineColour,
+            style: BorderStyle.solid,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
