ae5f76ef352513a6e66a704be5837604e2ecc538
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-14 19:12:26 +0200

    update: style changes to FormTextField
    Default border set to width of 2, as well as change the colour of text field when focused

--- lib/screens/components/form_text_field.dart
+++ lib/screens/components/form_text_field.dart
@@ -18,17 +18,36 @@ class FormTextField extends StatelessWidget {
       decoration: InputDecoration(
         labelText: labelText,
         prefixIcon: Icon(prefixIcon),
-        labelStyle: const TextStyle(color: secondaryTextColour),
+        labelStyle: const TextStyle(
+          color: secondaryTextColour,
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 16,
+        ),
         border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(100)),
-          borderSide: BorderSide(color: outlineColour),
+          borderSide: BorderSide(
+            color: outlineColour,
+            width: 2,
+          ),
         ),
         enabledBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(100)),
-          borderSide: BorderSide(color: outlineColour),
+          borderSide: BorderSide(
+            color: outlineColour,
+            width: 2,
+          ),
         ),
-        contentPadding:
-            const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: primaryColour,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(100),
+          ),
+        ),
       ),
     );
   }
