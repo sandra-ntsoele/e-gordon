a803aac2263fadfcc66576b0f5afd3527208c69d
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-14 16:39:08 +0200

    created: screen for new password creation

---
+++ lib/screens/password_recovery/new_password.dart
@@ -0,0 +1,53 @@
+import 'package:e_gordon/screens/components/form_text_field.dart';
+import 'package:e_gordon/screens/components/main_text.dart';
+import 'package:e_gordon/screens/components/password_rule.dart';
+import 'package:e_gordon/screens/components/rounded_button.dart';
+import 'package:e_gordon/screens/components/secondary_text.dart';
+import 'package:flutter/material.dart';
+
+class NewPassword extends StatelessWidget {
+  const NewPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // SECTION: Heading Text
+          MainText(displayText: "Reset your password"),
+          SecondaryText(displayText: "Please enter your new password"),
+          SizedBox(height: size.height * 0.03),
+          // SECTION: New password Input field
+          FormTextField(
+            labelText: "Password",
+            prefixIcon: Icons.lock_outline,
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          // SECTION: Password rules
+          Column(
+            children: [
+              PasswordRule(rule: "Atleast 8 characters,"),
+              PasswordRule(rule: "Including 1 letter,"),
+              PasswordRule(rule: "And 1 number"),
+            ],
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          // SECTION: Submission button
+          RoundedButton(
+            text: "Done",
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
