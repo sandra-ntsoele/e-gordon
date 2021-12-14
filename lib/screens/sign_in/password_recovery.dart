4208b9f82485ef81ad622ed88973f3e151ae3bd1
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-14 16:08:18 +0200

    created: password recovery screen
    design of screen is complete

---
+++ lib/screens/sign_in/password_recovery.dart
@@ -0,0 +1,42 @@
+import 'package:e_gordon/screens/components/form_text_field.dart';
+import 'package:e_gordon/screens/components/main_text.dart';
+import 'package:e_gordon/screens/components/rounded_button.dart';
+import 'package:e_gordon/screens/components/secondary_text.dart';
+import 'package:flutter/material.dart';
+
+class PasswordRecovery extends StatelessWidget {
+  const PasswordRecovery({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // SECTION: Heading text
+          MainText(displayText: "Password recovery"),
+          SecondaryText(
+              displayText: "Enter your email to recover your password"),
+          SizedBox(height: size.height * 0.03),
+          // SECTION: Input field
+          FormTextField(
+            labelText: "Email or phone number",
+            prefixIcon: Icons.email_outlined,
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          // SECTION: Submit button
+          RoundedButton(
+            text: "Sign In",
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
