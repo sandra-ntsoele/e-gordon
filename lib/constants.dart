676d0fcce647c5c65119962fc2a488b662b2908a
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-14 10:41:53 +0200

    update: added secondary colour for forms

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,6 +6,7 @@ const Color secondaryColour = Color.fromRGBO(255, 100, 100, 1);
 const Color mainTextColour = Color.fromRGBO(41, 62, 93, 1);
 const Color secondaryTextColour = Color.fromRGBO(159, 165, 192, 1);
 const Color outlineColour = Color.fromRGBO(208, 219, 234, 1);
+const Color secondaryBtnColour = Color.fromRGBO(244, 245, 247, 1);
 
 // Text styles
 const double headingOne = 22;
