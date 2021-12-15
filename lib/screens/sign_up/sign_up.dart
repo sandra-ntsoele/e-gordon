8995bb548275f211253342d277dbc9ac722e95a8
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-15 17:32:35 +0200

    update: wrap scren content with Scaffold

--- lib/screens/sign_up/sign_up.dart
+++ lib/screens/sign_up/sign_up.dart
@@ -15,55 +15,57 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return SizedBox(
-      height: size.height,
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // SECTION: Screen heading
-          MainText(displayText: "Welcome!"),
-          SecondaryText(displayText: "Please enter account details"),
-          // SECTION: Input fields
-          FormTextField(
-            labelText: "Email or phone number",
-            prefixIcon: Icons.email_outlined,
-          ),
-          PasswordField(
-            labelText: "Password",
-            prefixIcon: Icons.lock_outline,
-          ),
-          // SECTION: Password rules
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Your password must contain:",
-                style: TextStyle(
-                  color: mainTextColour,
-                  fontSize: paragraphOne,
-                  fontWeight: FontWeight.bold,
-                  height: 2.5,
+    return Scaffold(
+      body: SizedBox(
+        height: size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // SECTION: Screen heading
+            MainText(displayText: "Welcome!"),
+            SecondaryText(displayText: "Please enter account details"),
+            // SECTION: Input fields
+            FormTextField(
+              labelText: "Email or phone number",
+              prefixIcon: Icons.email_outlined,
+            ),
+            PasswordField(
+              labelText: "Password",
+              prefixIcon: Icons.lock_outline,
+            ),
+            // SECTION: Password rules
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Your password must contain:",
+                  style: TextStyle(
+                    color: mainTextColour,
+                    fontSize: paragraphOne,
+                    fontWeight: FontWeight.bold,
+                    height: 2.5,
+                  ),
                 ),
-              ),
-              Column(
-                children: [
-                  PasswordRule(rule: "Atleast 8 characters,"),
-                  PasswordRule(rule: "Including 1 letter,"),
-                  PasswordRule(rule: "And 1 number"),
-                ],
-              )
-            ],
-          ),
-          // SECTION: Submit button
-          SizedBox(
-            height: size.height * 0.05,
-          ),
-          RoundedButton(
-            text: "Sign Up",
-            onPressed: () {},
-          ),
-        ],
+                Column(
+                  children: [
+                    PasswordRule(rule: "Atleast 8 characters,"),
+                    PasswordRule(rule: "Including 1 letter,"),
+                    PasswordRule(rule: "And 1 number"),
+                  ],
+                )
+              ],
+            ),
+            // SECTION: Submit button
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            RoundedButton(
+              text: "Sign Up",
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
