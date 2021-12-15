3ecfd0a7480e82549c3f4dcd393a07ce9e6396d6
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-15 09:43:55 +0200

    update: remove scaffold
    Scaffold is defined at the main file so it is unecessary in here

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -9,53 +9,51 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Container(
-        height: size.height,
-        width: double.infinity,
-        child: Column(children: [
-          Container(
-            height: 400,
-            child: Image.asset('assets/images/onboarding-old.png'),
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Column(children: [
+        Container(
+          height: 400,
+          child: Image.asset('assets/images/onboarding-old.png'),
+        ),
+        Column(children: [
+          const Text(
+            "e-Gordon",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              height: 1.5,
+            ),
           ),
-          Column(children: [
-            const Text(
-              "e-Gordon",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                height: 1.5,
-              ),
-            ),
-            SizedBox(
-              height: size.height * 0.03,
-            ),
-            const Text(
-              "Don't just stand there like a big fucking muffin.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                height: 1.5,
-                color: secondaryTextColour,
-              ),
-            ),
-          ]),
-          Container(
-            margin: const EdgeInsets.only(top: 72),
-            width: double.infinity,
-            padding: const EdgeInsets.only(left: 25, right: 25),
-            child: RoundedButton(
-              text: "Start cooking",
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignIn()),
-                );
-              },
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          const Text(
+            "Don't just stand there like a big fucking muffin.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              height: 1.5,
+              color: secondaryTextColour,
             ),
           ),
         ]),
-      ),
+        Container(
+          margin: const EdgeInsets.only(top: 72),
+          width: double.infinity,
+          padding: const EdgeInsets.only(left: 25, right: 25),
+          child: RoundedButton(
+            text: "Start cooking",
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SignIn()),
+              );
+            },
+          ),
+        ),
+      ]),
     );
   }
 }
