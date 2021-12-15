68b2db7ac8ee1fac1ecf21c6f76f7b2ab299adf2
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-15 14:51:05 +0200

    update: remove padding
    Padding removed because it will be controlled by the screen in which the field is being used

--- lib/screens/components/password_field.dart
+++ lib/screens/components/password_field.dart
@@ -14,31 +14,28 @@ class PasswordField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-      child: TextFormField(
-        obscureText: true,
-        decoration: InputDecoration(
-          labelText: labelText,
-          prefixIcon: Icon(prefixIcon),
-          labelStyle: const TextStyle(color: secondaryTextColour),
-          border: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(100)),
-            borderSide: BorderSide(color: outlineColour),
-          ),
-          enabledBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(100)),
-            borderSide: BorderSide(color: outlineColour),
-          ),
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: labelText,
+        prefixIcon: Icon(prefixIcon),
+        labelStyle: const TextStyle(color: secondaryTextColour),
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(100)),
+          borderSide: BorderSide(color: outlineColour),
         ),
-        validator: (String? value) {
-          if (value!.trim().isEmpty) {
-            return "password is required";
-          }
-        },
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(100)),
+          borderSide: BorderSide(color: outlineColour),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
       ),
+      validator: (String? value) {
+        if (value!.trim().isEmpty) {
+          return "password is required";
+        }
+      },
     );
   }
 }
