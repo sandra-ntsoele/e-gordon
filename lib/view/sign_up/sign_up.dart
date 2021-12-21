9df79799268c6644fb255919d51678e778081131
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-21 18:36:32 +0200

    update: wrap form fields with Form widget

--- lib/view/sign_up/sign_up.dart
+++ lib/view/sign_up/sign_up.dart
@@ -19,6 +19,7 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final _formKey = GlobalKey<FormState>();
 
     return Scaffold(
       body: SizedBox(
@@ -26,83 +27,86 @@ class _SignUpState extends State<SignUp> {
         width: double.infinity,
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // SECTION: Screen heading
-                const Heading(
-                  text: "Welcome!",
-                  headingType: 1,
-                ),
-                const Paragraph(
-                  text: "Please enter account details",
-                  paragraphType: 2,
-                ),
-                SizedBox(
-                  height: size.height * 0.03,
-                ),
-                // SECTION: Input fields
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 25,
-                    vertical: 25,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // SECTION: Screen heading
+                  const Heading(
+                    text: "Welcome!",
+                    headingType: 1,
                   ),
-                  child: Column(
-                    children: [
-                      FormTextField(
-                        labelText: "Email or phone number",
-                        prefixIcon: Icons.email_outlined,
-                      ),
-                      SizedBox(
-                        height: size.height * 0.03,
-                      ),
-                      PasswordField(
-                        labelText: "Password",
-                        prefixIcon: Icons.lock_outline,
-                      ),
-                    ],
+                  const Paragraph(
+                    text: "Please enter account details",
+                    paragraphType: 2,
                   ),
-                ),
-                // SECTION: Password rules
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 25,
-                    vertical: 25,
+                  SizedBox(
+                    height: size.height * 0.03,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Your password must contain:",
-                        style: TextStyle(
-                          color: mainTextColour,
-                          fontSize: paragraphOne,
-                          fontWeight: FontWeight.bold,
-                          height: 2.5,
+                  // SECTION: Input fields
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 25,
+                    ),
+                    child: Column(
+                      children: [
+                        FormTextField(
+                          labelText: "Email or phone number",
+                          prefixIcon: Icons.email_outlined,
                         ),
-                      ),
-                      Column(
-                        children: [
-                          PasswordRule(rule: "Atleast 8 characters,"),
-                          PasswordRule(rule: "Including 1 letter,"),
-                          PasswordRule(rule: "And 1 number"),
-                        ],
-                      )
-                    ],
+                        SizedBox(
+                          height: size.height * 0.03,
+                        ),
+                        PasswordField(
+                          labelText: "Password",
+                          prefixIcon: Icons.lock_outline,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                // SECTION: Submit button
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 25,
-                    vertical: size.height * 0.03,
+                  // SECTION: Password rules
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 25,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Your password must contain:",
+                          style: TextStyle(
+                            color: mainTextColour,
+                            fontSize: paragraphOne,
+                            fontWeight: FontWeight.bold,
+                            height: 2.5,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            PasswordRule(rule: "Atleast 8 characters,"),
+                            PasswordRule(rule: "Including 1 letter,"),
+                            PasswordRule(rule: "And 1 number"),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                  child: RoundedButton(
-                    text: "Sign Up",
-                    onPressed: () {},
+                  // SECTION: Submit button
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: size.height * 0.03,
+                    ),
+                    child: RoundedButton(
+                      text: "Sign Up",
+                      onPressed: () {},
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
