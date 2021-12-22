608d682d2384d56aa4c4fe4e526d2ae85059efbe
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-22 16:15:26 +0200

    added: FlutterFire initialisation code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:e_gordon/view/components/rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'view/onboarding/onboarding.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
