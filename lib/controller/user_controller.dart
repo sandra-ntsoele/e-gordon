5334860cea0e23301221df7f0317b014ef51bd52
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-24 15:11:27 +0200

    created: a controller class for users

---
+++ lib/controller/user_controller.dart
@@ -0,0 +1,61 @@
+import 'package:email_validator/email_validator.dart';
+
+class UserController {
+  /* 
+   * Class methods 
+   */
+
+  static String? validateEmail(String? userInput) {
+    if (userInput == null || userInput.isEmpty) {
+      return "Email cannot be empty";
+    } else if (!EmailValidator.validate(userInput)) {
+      return "Invalid email";
+    } else {
+      // _email = userInput;
+      return null;
+    }
+  }
+
+  static String? validatePassword(String userInput) {
+    /* 
+      r'^
+      (?=.*[A-Z])       // should contain at least one upper case
+      (?=.*[a-z])       // should contain at least one lower case
+      (?=.*?[0-9])          // should contain at least one digit
+      (?=.*?[!@#\$&*~]).{8,}  // should contain at least one Special character
+      $ 
+    */
+    String upperCaseChecker = "(?=.*[A-Z])";
+    String lowerCaseChecker = "(?=.*[a-z])";
+    String digitChecker = "(?=.*?[0-9])";
+    String specialCharChecker = "(?=.*?[!@#\$&*~])";
+    String lengthChecker = ".{8,32} ";
+
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+
+    RegExp regExp = RegExp(pattern);
+
+    if (userInput.isEmpty) {
+      return "Password cannot be empty";
+    } else if (!regExp.hasMatch(userInput)) {
+      // If password does not meet alll rules. Investigate what is missing
+      if (!RegExp(upperCaseChecker).hasMatch(userInput)) {
+        return "You need one uppercase letter";
+      } else if (!RegExp(lowerCaseChecker).hasMatch(userInput)) {
+        return "You need one lowercase letter";
+      } else if (!RegExp(digitChecker).hasMatch(userInput)) {
+        return "You need one number";
+      } else if (!RegExp(specialCharChecker).hasMatch(userInput)) {
+        return "You need one special character";
+      } else if (!RegExp(lengthChecker).hasMatch(userInput)) {
+        return "Must be 8 characters or more";
+      } else {
+        return "Enter a valid password";
+      }
+    } else {
+      // _password = userInput;
+      return null;
+    }
+  }
+}
