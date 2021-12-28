be171bef31bbce5cb36958dd135381d752a1ffc1
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-28 20:23:40 +0200

    created: a verification screen
    Screen shown after a user creates an account. The user will be kept here till email is verified

---
+++ lib/view/verify/verify.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:e_gordon/view/components/text_components/heading.dart';
+import 'package:e_gordon/view/components/text_components/paragraph.dart';
+import 'package:e_gordon/view/profile/profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmail extends StatefulWidget {
+  const VerifyEmail({Key? key}) : super(key: key);
+
+  @override
+  _VerifyEmailState createState() => _VerifyEmailState();
+}
+
+class _VerifyEmailState extends State<VerifyEmail> {
+  final auth = FirebaseAuth.instance;
+  User? user;
+  Timer? timer;
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      checkEmailVerified();
+    });
+
+    super.initState();
+  }
+
+  // @override
+  // void dispose() {
+  //   timer!.cancel();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: size.height,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Heading(
+                text: "Verify your email to use\ne-gordon",
+                headingType: 1,
+              ),
+              Paragraph(
+                text: "We sent a verification link to your@email.com",
+                paragraphType: 2,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> checkEmailVerified() async {
+    user = auth.currentUser;
+    await user!.reload();
+
+    if (user!.emailVerified) {
+      timer!.cancel();
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MyProfile(),
+        ),
+      );
+    }
+  }
+}
