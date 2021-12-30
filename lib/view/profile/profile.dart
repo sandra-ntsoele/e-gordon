9fe38b36229407138e0feebc34c9b13a5263f9a0
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-30 19:20:33 +0200

    updated: layout structure
    Making changes to the widget structure in order to use tabs in the body

--- lib/view/profile/profile.dart
+++ lib/view/profile/profile.dart
@@ -1,3 +1,4 @@
+import 'package:e_gordon/view/components/profile_header.dart';
 import 'package:e_gordon/view/components/text_components/heading.dart';
 import 'package:e_gordon/view/components/text_components/paragraph.dart';
 import 'package:e_gordon/view/constants.dart';
@@ -11,90 +12,103 @@ class MyProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.home_filled),
-        backgroundColor: primaryColour,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (value) => {},
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.create_rounded),
-            label: "Create",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(null),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-          ),
-        ],
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        height: size.height,
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: size.height * 0.08,
-              ),
-              /* Avatar */
-              CircleAvatar(
-                backgroundImage: const NetworkImage("https://bit.ly/3zcOVBL"),
-                radius: size.height * 0.06,
-                backgroundColor: outlineColour,
-              ),
-              SizedBox(height: size.height * 0.03),
-              /* User Name */
-              const Heading(
-                text: "Gordon Ramsay",
-                headingType: 2,
-              ),
-              SizedBox(height: size.height * 0.03),
-              /* Profile stats */
-              Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: const [
-                        Heading(text: "32", headingType: 3),
-                        Paragraph(text: "Recipes", paragraphType: 2),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: const [
-                        Heading(text: "32", headingType: 3),
-                        Paragraph(text: "Following", paragraphType: 2),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: const [
-                        Heading(text: "32", headingType: 3),
-                        Paragraph(text: "Followers", paragraphType: 2),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Divider(
-                height: size.height * 0.05,
-                thickness: 5,
-                color: outlineColour,
-              ),
-            ],
-          ),
-        ),
+    return DefaultTabController(
+      length: 2,
+      child: NestedScrollView(
+        headerSliverBuilder: (context, index) {
+          return const [
+            // Profile header
+            SliverToBoxAdapter(child: ProfileHeader())
+          ];
+        },
+        body: Text("Hello tabs"),
       ),
     );
+
+    // Scaffold(
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: () {},
+    //     child: const Icon(Icons.home_filled),
+    //     backgroundColor: primaryColour,
+    //   ),
+    //   floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    //   bottomNavigationBar: BottomNavigationBar(
+    //     onTap: (value) => {},
+    //     items: const [
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.create_rounded),
+    //         label: "Create",
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(null),
+    //         label: "Home",
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.person),
+    //         label: "Profile",
+    //       ),
+    //     ],
+    //   ),
+    //   body: SizedBox(
+    //     width: double.infinity,
+    //     height: size.height,
+    //     child: Center(
+    //       child: Column(
+    //         children: [
+    //           SizedBox(
+    //             height: size.height * 0.08,
+    //           ),
+    //           /* Avatar */
+    //           CircleAvatar(
+    //             backgroundImage: const NetworkImage("https://bit.ly/3zcOVBL"),
+    //             radius: size.height * 0.06,
+    //             backgroundColor: outlineColour,
+    //           ),
+    //           SizedBox(height: size.height * 0.03),
+    //           /* User Name */
+    //           const Heading(
+    //             text: "Gordon Ramsay",
+    //             headingType: 2,
+    //           ),
+    //           SizedBox(height: size.height * 0.03),
+    //           /* Profile stats */
+    //           Row(
+    //             children: [
+    //               Expanded(
+    //                 child: Column(
+    //                   children: const [
+    //                     Heading(text: "32", headingType: 3),
+    //                     Paragraph(text: "Recipes", paragraphType: 2),
+    //                   ],
+    //                 ),
+    //               ),
+    //               Expanded(
+    //                 child: Column(
+    //                   children: const [
+    //                     Heading(text: "32", headingType: 3),
+    //                     Paragraph(text: "Following", paragraphType: 2),
+    //                   ],
+    //                 ),
+    //               ),
+    //               Expanded(
+    //                 child: Column(
+    //                   children: const [
+    //                     Heading(text: "32", headingType: 3),
+    //                     Paragraph(text: "Followers", paragraphType: 2),
+    //                   ],
+    //                 ),
+    //               )
+    //             ],
+    //           ),
+    //           Divider(
+    //             height: size.height * 0.05,
+    //             thickness: 5,
+    //             color: outlineColour,
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
