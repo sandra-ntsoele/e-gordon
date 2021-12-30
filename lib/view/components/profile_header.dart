c015ff1fbda46d375747ba6dbc0d2e1283a0727d
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-30 19:21:35 +0200

    created: a profile header widget

---
+++ lib/view/components/profile_header.dart
@@ -0,0 +1,53 @@
+import 'package:e_gordon/view/components/profile_statistic.dart';
+import 'package:e_gordon/view/components/text_components/heading.dart';
+import 'package:e_gordon/view/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatefulWidget {
+  const ProfileHeader({Key? key}) : super(key: key);
+
+  @override
+  _ProfileHeaderState createState() => _ProfileHeaderState();
+}
+
+class _ProfileHeaderState extends State<ProfileHeader> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundImage: const NetworkImage("https://bit.ly/3zcOVBL"),
+          radius: size.height * 0.06,
+          backgroundColor: outlineColour,
+        ),
+        SizedBox(height: size.height * 0.03),
+        /* User Name */
+        const Heading(
+          text: "Gordon Ramsay",
+          headingType: 2,
+        ),
+        SizedBox(height: size.height * 0.03),
+        /* Profile stats */
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            ProfileStatistic(
+              label: "Recipes",
+              figure: "32",
+            ),
+            ProfileStatistic(
+              label: "Followers",
+              figure: "1287",
+            ),
+            ProfileStatistic(
+              label: "Following",
+              figure: "782",
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
