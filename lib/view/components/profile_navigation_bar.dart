3f409712d62b202741368441b66b2348d85effce
sandra-ntsoele <sandra.ntsoele@pm.me>
2021-12-31 14:31:38 +0200

    update: extracted the bottom navigation bar to a seperate file

---
+++ lib/view/components/profile_navigation_bar.dart
@@ -0,0 +1,41 @@
+import 'package:e_gordon/view/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProfileNavigationBar extends StatelessWidget {
+  const ProfileNavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.create,
+          ),
+          label: "Upload",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(null),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+          ),
+          label: "Profile",
+        ),
+      ],
+      selectedItemColor: primaryColour,
+      selectedLabelStyle: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: smallText,
+      ),
+      unselectedLabelStyle: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: smallText,
+      ),
+    );
+  }
+}
--- lib/view/profile/profile.dart
+++ lib/view/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:e_gordon/view/components/profile_header.dart';
+import 'package:e_gordon/view/components/profile_navigation_bar.dart';
 import 'package:e_gordon/view/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,35 +15,7 @@ class MyProfile extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.create,
-            ),
-            label: "Upload",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(null),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-            ),
-            label: "Profile",
-          ),
-        ],
-        selectedItemColor: primaryColour,
-        selectedLabelStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: smallText,
-        ),
-        unselectedLabelStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: smallText,
-        ),
-      ),
+      bottomNavigationBar: const ProfileNavigationBar(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.home_filled),
         backgroundColor: primaryColour,
@@ -77,91 +50,5 @@ class MyProfile extends StatelessWidget {
         ),
       ),
     );
-
-    // Scaffold(
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: () {},
-    //     child: const Icon(Icons.home_filled),
-    //     backgroundColor: primaryColour,
-    //   ),
-    //   floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    //   bottomNavigationBar: BottomNavigationBar(
-    //     onTap: (value) => {},
-    //     items: const [
-    //       BottomNavigationBarItem(
-    //         icon: Icon(Icons.create_rounded),
-    //         label: "Create",
-    //       ),
-    //       BottomNavigationBarItem(
-    //         icon: Icon(null),
-    //         label: "Home",
-    //       ),
-    //       BottomNavigationBarItem(
-    //         icon: Icon(Icons.person),
-    //         label: "Profile",
-    //       ),
-    //     ],
-    //   ),
-    //   body: SizedBox(
-    //     width: double.infinity,
-    //     height: size.height,
-    //     child: Center(
-    //       child: Column(
-    //         children: [
-    //           SizedBox(
-    //             height: size.height * 0.08,
-    //           ),
-    //           /* Avatar */
-    //           CircleAvatar(
-    //             backgroundImage: const NetworkImage("https://bit.ly/3zcOVBL"),
-    //             radius: size.height * 0.06,
-    //             backgroundColor: outlineColour,
-    //           ),
-    //           SizedBox(height: size.height * 0.03),
-    //           /* User Name */
-    //           const Heading(
-    //             text: "Gordon Ramsay",
-    //             headingType: 2,
-    //           ),
-    //           SizedBox(height: size.height * 0.03),
-    //           /* Profile stats */
-    //           Row(
-    //             children: [
-    //               Expanded(
-    //                 child: Column(
-    //                   children: const [
-    //                     Heading(text: "32", headingType: 3),
-    //                     Paragraph(text: "Recipes", paragraphType: 2),
-    //                   ],
-    //                 ),
-    //               ),
-    //               Expanded(
-    //                 child: Column(
-    //                   children: const [
-    //                     Heading(text: "32", headingType: 3),
-    //                     Paragraph(text: "Following", paragraphType: 2),
-    //                   ],
-    //                 ),
-    //               ),
-    //               Expanded(
-    //                 child: Column(
-    //                   children: const [
-    //                     Heading(text: "32", headingType: 3),
-    //                     Paragraph(text: "Followers", paragraphType: 2),
-    //                   ],
-    //                 ),
-    //               )
-    //             ],
-    //           ),
-    //           Divider(
-    //             height: size.height * 0.05,
-    //             thickness: 5,
-    //             color: outlineColour,
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
