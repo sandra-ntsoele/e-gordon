6383a6be021b7b88540d6bc0f9b393593d78b8c7
sandra-ntsoele <sandra.ntsoele@pm.me>
2022-01-06 13:40:38 +0200

    style: extracted code into seperate methods
    For better readability I've extracted the code for different sections of the screen into seperate methods

--- lib/view/sign_up/sign_up.dart
+++ lib/view/sign_up/sign_up.dart
@@ -33,98 +33,111 @@ class _SignUpState extends State<SignUp> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: SizedBox(
-        height: size.height,
-        width: double.infinity,
-        child: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // SECTION: Screen heading
-                  Heading(
-                    text: "Welcome!",
-                    headingType: 1,
-                    overflow: false,
-                  ),
-                  const Paragraph(
-                    text: "Please enter account details",
-                    paragraphType: 2,
-                  ),
-                  SizedBox(
-                    height: size.height * 0.03,
-                  ),
-                  // SECTION: Input fields
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 25,
-                      vertical: 25,
-                    ),
-                    child: Column(
-                      children: [
-                        EmailTextField(
-                          formKey: _formKey,
-                          textEditingController: emailController,
-                        ),
-                        SizedBox(height: size.height * 0.03),
-                        PasswordField(
-                          formKey: _formKey,
-                          textEditingController: passwordController,
-                        ),
-                      ],
-                    ),
-                  ),
-                  // SECTION: Password rules
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 25,
-                      vertical: 25,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Your password must contain:",
-                          style: TextStyle(
-                            color: mainTextColour,
-                            fontSize: paragraphOne,
-                            fontWeight: FontWeight.bold,
-                            height: 2.5,
-                          ),
-                        ),
-                        Column(
-                          children: const [
-                            PasswordRule(rule: "Atleast 8 characters,"),
-                            PasswordRule(rule: "Including 1 letter,"),
-                            PasswordRule(rule: "And 1 number"),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  // SECTION: Submit button
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 25,
-                      vertical: size.height * 0.03,
-                    ),
-                    child: RoundedButton(
-                      text: "Sign Up",
-                      onPressed: () => UserController.registerUser(
-                        context,
-                        email: emailController.text,
-                        password: passwordController.text,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                screenHeader(size),
+                inputFields(size),
+                passwordRules(),
+                signUpBtn(size, context),
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Padding signUpBtn(Size size, BuildContext context) {
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 25,
+        vertical: size.height * 0.03,
+      ),
+      child: RoundedButton(
+        text: "Sign Up",
+        onPressed: () => UserController.registerUser(
+          context,
+          email: email,
+          password: password,
+        ),
+      ),
+    );
+  }
+
+  Padding passwordRules() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25,
+        vertical: 25,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Your password must contain:",
+            style: TextStyle(
+              color: mainTextColour,
+              fontSize: paragraphOne,
+              fontWeight: FontWeight.bold,
+              height: 2.5,
+            ),
+          ),
+          Column(
+            children: const [
+              PasswordRule(rule: "Atleast 8 characters,"),
+              PasswordRule(rule: "Including 1 letter,"),
+              PasswordRule(rule: "And 1 number"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Padding inputFields(Size size) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25,
+        vertical: 25,
+      ),
+      child: Column(
+        children: [
+          EmailTextField(
+            formKey: _formKey,
+            textEditingController: emailController,
+          ),
+          SizedBox(height: size.height * 0.03),
+          PasswordField(
+            formKey: _formKey,
+            textEditingController: passwordController,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget screenHeader(size) {
+    return Column(
+      children: [
+        Heading(
+          text: "Welcome!",
+          headingType: 1,
+          overflow: false,
+        ),
+        const Paragraph(
+          text: "Please enter account details",
+          paragraphType: 2,
+        ),
+        SizedBox(height: size.height * 0.03),
+      ],
     );
   }
 }
