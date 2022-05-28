b217573b173ee81f582d82a2b321cdad1c4e1578
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-05-28 12:34:50 +0200

    feat: add createAccount, signIn and signOut functionality

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,9 +1,63 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final _auth = FirebaseAuth.instance;
 
   // Listen to auth state changes
-  get authState => _auth.authStateChanges();
-  get userChanges => _auth.authStateChanges();
+  Stream<User?> get userStateStream => _auth.authStateChanges();
+
+  Stream<User?> get userChanges => _auth.userChanges();
+
+  String? validateEmail(String? input) {
+    if (input == null || input.isEmpty) {
+      return "Email or phone number cannot be empty";
+    } else if (!EmailValidator.validate(input)) {
+      return "Invalid email";
+    }
+    return null;
+  }
+
+  // Creating a user with email and password
+  Future<String> createAccount(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message.toString();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message.toString();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String> signOut() async {
+    try {
+      await _auth.signOut();
+
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message.toString();
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
