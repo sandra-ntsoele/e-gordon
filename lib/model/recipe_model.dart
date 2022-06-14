3b90ec218e1d4c65b65a14cc4700b7816d638299
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-06-14 21:36:03 +0200

    feat: add logic for uploading recipe photo to model

--- lib/model/recipe_model.dart
+++ lib/model/recipe_model.dart
@@ -1,22 +1,86 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
 
-class RecipeModel {
-  String? rcpName;
-  String? rcpDescription;
-  List? rcpDirections;
-  int? rcpCookTime;
-  int? rcpServings;
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
 
-  Future<void> createRecipe() {
-    CollectionReference recipes =
-        FirebaseFirestore.instance.collection("recipes");
+class Recipes {
+  String recipe;
+  String description;
+  String photoURL;
+  int? rating;
+  int chefID;
+  int categoryID;
+  int dietID;
 
-    return recipes.add({
-      "RcpName": rcpName,
-      "RcpDescription": rcpDescription,
-      "RcpDirections": rcpDirections,
-      "RcpCookTime": rcpCookTime,
-      "RcpServings": rcpServings
-    });
+  Recipes({
+    required this.recipe,
+    required this.description,
+    required this.photoURL,
+    this.rating,
+    required this.categoryID,
+    required this.chefID,
+    required this.dietID,
+  });
+
+  static SupabaseClient supabase = Supabase.instance.client;
+
+  Future<String?> _uploadRecipePhoto(PlatformFile image) async {
+    // Upload to bucket
+    String path = image.path.toString();
+    String fileName = image.name;
+    int fileNameLength = fileName.length;
+    String? extension = image.extension;
+    int extensionLength = image.extension!.length.toInt() + 1;
+    final parsedFileName = fileName.characters
+        .take(fileNameLength - extensionLength); // File name sans extension
+
+    File photo = File(path);
+    String uuid = const Uuid().v1();
+    String finalFileName = "$parsedFileName-$uuid.$extension";
+
+    StorageResponse<String> res =
+        await supabase.storage.from('recipe-files').upload(
+              'photos/$finalFileName',
+              photo,
+            );
+
+    if (res.hasError) {
+      return res.error?.message;
+    } else {
+      String? publicURL = supabase.storage
+          .from('recipe-files')
+          .getPublicUrl(finalFileName)
+          .data;
+
+      return publicURL;
+    }
   }
+
+  void createRecipe(PlatformFile image) async {
+    try {
+      // First upload photo to get URL
+      _uploadRecipePhoto(image);
+      final response = await supabase.from("recipes").insert([
+        {
+          'recipe': recipe,
+          'description': description,
+          'photoURL': photoURL,
+          'rating': rating,
+          'categoryID': categoryID,
+          'chefID': chefID,
+          'dietID': dietID,
+        }
+      ]).execute();
+
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
+  // TODO: GET recipe method
+  // TODO: UPDATE recipe method
+  // TODO: DELETE recipe method
+
 }
