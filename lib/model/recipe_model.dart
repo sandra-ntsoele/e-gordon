5d57c897c388ef462a87c5882e47910a1ef31073
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-06-26 14:29:24 +0200

    feat: define recipe model

--- lib/model/recipe_model.dart
+++ lib/model/recipe_model.dart
@@ -1,32 +1,88 @@
 import 'dart:io';
 
+import 'package:e_gordon/services/auth_service.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uuid/uuid.dart';
 
 class Recipes {
+  /* 
+    Class fields 
+  */
   String recipe;
   String description;
-  String photoURL;
-  int? rating;
-  int chefID;
-  int categoryID;
-  int dietID;
+  String category;
+  String diet;
+  PlatformFile image;
 
-  Recipes({
-    required this.recipe,
-    required this.description,
-    required this.photoURL,
-    this.rating,
-    required this.categoryID,
-    required this.chefID,
-    required this.dietID,
-  });
+  late String? chefID;
+  late int categoryID;
+  late int dietID;
+  late String photoURL;
 
   static SupabaseClient supabase = Supabase.instance.client;
 
-  Future<String?> _uploadRecipePhoto(PlatformFile image) async {
+  /* 
+   * Getters/Setters
+  */
+  late Map data;
+
+  /* 
+   * Constructor
+  */
+  Recipes({
+    required this.recipe,
+    required this.description,
+    required this.category,
+    required this.diet,
+    required this.image,
+  }) {
+    chefID = AuthService().currentUser?.uid; // Initialise chefID
+
+    // Initialise categoryID
+    _addRecipeCategory().then((response) async {
+      if (response == "23505") {
+        // Category already exists so find existing id
+        categoryID = await _getCategoryID();
+      } else {
+        // If insert was successful get the new ID
+        categoryID = response;
+      }
+    });
+
+    print(categoryID);
+
+    // Initialise dietID
+    _addDiet().then(
+      (response) async {
+        if (response == "23505") {
+          // Diet already exists so find existing id
+          dietID = await _getDietID();
+        } else {
+          // If insert was successful get the new ID
+          dietID = response;
+        }
+      },
+    );
+
+    // Initialise photoURL
+    _uploadRecipePhoto().then(
+      (response) {
+        if (response.runtimeType == StorageError) {
+          throw response;
+        } else {
+          photoURL = response;
+        }
+      },
+    );
+  }
+
+  /* 
+    Class methods 
+  */
+  Future<dynamic> _uploadRecipePhoto() async {
     // Upload to bucket
     String path = image.path.toString();
     String fileName = image.name;
@@ -47,7 +103,7 @@ class Recipes {
             );
 
     if (res.hasError) {
-      return res.error?.message;
+      return res.error;
     } else {
       String? publicURL = supabase.storage
           .from('recipe-files')
@@ -58,16 +114,58 @@ class Recipes {
     }
   }
 
-  void createRecipe(PlatformFile image) async {
+  Future<int> _getCategoryID() async {
+    PostgrestResponse<dynamic> response = await supabase
+        .from("recipecategories")
+        .select("categoryid")
+        .eq("category", category)
+        .execute();
+
+    return response.data[0]["categoryid"];
+  }
+
+  Future<dynamic> _addRecipeCategory() async {
+    PostgrestResponse<dynamic> response = await supabase
+        .from("recipecategories")
+        .insert({'category': category},
+            returning: ReturningOption.representation).execute();
+
+    if (response.hasError) {
+      return response.error?.code;
+    } else {
+      return response.data[0]["categoryid"];
+    }
+  }
+
+  Future<dynamic> _addDiet() async {
+    PostgrestResponse<dynamic> response = await supabase.from("diets").insert(
+        {'diet': diet},
+        returning: ReturningOption.representation).execute();
+
+    if (response.hasError) {
+      return response.error?.code;
+    } else {
+      return response.data[0]["dietid"];
+    }
+  }
+
+  Future<int> _getDietID() async {
+    PostgrestResponse<dynamic> response = await supabase
+        .from("recipecategories")
+        .select("categoryid")
+        .eq("category", category)
+        .execute();
+
+    return response.data[0]["categoryid"];
+  }
+
+  void createRecipe() async {
     try {
-      // First upload photo to get URL
-      _uploadRecipePhoto(image);
       final response = await supabase.from("recipes").insert([
         {
           'recipe': recipe,
           'description': description,
           'photoURL': photoURL,
-          'rating': rating,
           'categoryID': categoryID,
           'chefID': chefID,
           'dietID': dietID,
@@ -79,8 +177,4 @@ class Recipes {
       print(e);
     }
   }
-  // TODO: GET recipe method
-  // TODO: UPDATE recipe method
-  // TODO: DELETE recipe method
-
 }
