abd7337c0f395d54ed101f761ed8cc5941e283c9
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-06-26 14:06:54 +0200

    feat: sync user to supabase when email is verified

--- lib/view/verify/verify.dart
+++ lib/view/verify/verify.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:e_gordon/model/user_model.dart';
 import 'package:e_gordon/services/auth_service.dart';
+import 'package:e_gordon/services/supabase.dart';
 import 'package:e_gordon/view/components/text_components/heading.dart';
 import 'package:e_gordon/view/components/text_components/paragraph.dart';
 import 'package:flutter/material.dart';
@@ -18,16 +19,29 @@ class VerifyEmail extends StatefulWidget {
 class _VerifyEmailState extends State<VerifyEmail> {
   Timer? timer;
   final AuthService _authService = AuthService();
+  final SupabaseHelper _supabaseHelper = SupabaseHelper();
 
   @override
   void initState() {
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      // Check if email is verified
-      if (_authService.emailIsVerified) {
-        Navigator.of(context).pushNamed("/explore");
-        timer.cancel();
-      }
-    });
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) async {
+        await _authService.currentUser!.reload();
+        var currentUser = _authService.currentUser;
+
+        if (currentUser!.emailVerified) {
+          timer.cancel();
+          var response = await _supabaseHelper.syncUser(currentUser);
+          if (response!.hasError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(response.error!.message)),
+            );
+          } else {
+            Navigator.of(context).pushNamed("/index");
+          }
+        }
+      },
+    );
 
     super.initState();
   }
