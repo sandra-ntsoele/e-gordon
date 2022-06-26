85767a08be1268650fa158cec6b2555f74e7d11d
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-06-26 14:05:28 +0200

    refactor: remove supabase initializer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:e_gordon/custom_theme.dart';
-import 'package:e_gordon/view/explore_page/explore_page.dart';
 import 'package:e_gordon/view/index.dart';
 import 'package:e_gordon/view/profile/profile.dart';
 import 'package:e_gordon/view/registration/registration_screen.dart';
@@ -7,7 +6,6 @@ import 'package:e_gordon/view/sign_in/sign_in.dart';
 import 'package:e_gordon/view/verify/verify.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'view/splash_screen/splash_screen.dart';
 
 void main() async {
@@ -15,10 +13,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-  await Supabase.initialize(
-      url: "https://igzvyjvoqclpdwvokngt.supabase.co",
-      anonKey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnenZ5anZvcWNscGR3dm9rbmd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTAyOTE5NjQsImV4cCI6MTk2NTg2Nzk2NH0.ogT2qbFL-4RxbzVWA_Ab2kTLOcGOcXRGOITjGCebHPA");
   runApp(const MyApp());
 }
 
@@ -41,7 +35,6 @@ class _MyAppState extends State<MyApp> {
         "/registration-verification": (context) => const VerifyEmail(),
         "/login": (context) => const SignIn(),
         "/index": (context) => const Index(),
-        "/explore": (context) => const ExplorePage(),
         "/profile": (context) => const MyProfile(),
       },
       initialRoute: "/",
