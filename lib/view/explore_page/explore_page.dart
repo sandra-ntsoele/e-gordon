ca0333319f5a1a023b4eba0907af75a305239a8d
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-06-26 21:23:37 +0200

    refactor: wrap explore page with a futurebuilder that uses a supabase stream

--- lib/view/explore_page/explore_page.dart
+++ lib/view/explore_page/explore_page.dart
@@ -1,9 +1,8 @@
 import 'package:e_gordon/custom_theme.dart';
-import 'package:e_gordon/services/auth_service.dart';
+import 'package:e_gordon/services/supabase.dart';
 import 'package:e_gordon/view/explore_page/components/category_chip_list_view.dart';
 import 'package:e_gordon/view/explore_page/components/horizontal_list_view.dart';
 import 'package:e_gordon/view/explore_page/explore_page_controller.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'components/recipe_collection_list_view.dart';
 
@@ -21,7 +20,8 @@ class ExplorePage extends StatefulWidget {
 class _ExplorePageState extends State<ExplorePage> {
   FocusNode focusNode = FocusNode();
   int recipeFilter = 0;
-  final User? currentUser = AuthService().user;
+  var supabaseHelper = SupabaseHelper();
+
   int pageIndex = 0;
 
   selectedCategory(int userChoice) {
@@ -36,22 +36,31 @@ class _ExplorePageState extends State<ExplorePage> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    if (currentUser == null) {
-      Navigator.of(context).pushNamed("/");
-    }
-
-    return Container(
-      color: CustomTheme.white,
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          const HorizontalListView(),
-          CategoryChipListView(onSelected: selectedCategory),
-          RecipeCollectionListView(filter: recipeFilter),
-        ],
-      ),
+    var stream = supabaseHelper.getSupabaseStream("diets", "diet");
+    return FutureBuilder(
+      future: stream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Container(
+            color: CustomTheme.white,
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                const HorizontalListView(),
+                CategoryChipListView(onSelected: selectedCategory),
+                RecipeCollectionListView(filter: recipeFilter),
+              ],
+            ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: CustomTheme.secondaryBittersweet,
+            ),
+          );
+        }
+      },
     );
   }
 }
